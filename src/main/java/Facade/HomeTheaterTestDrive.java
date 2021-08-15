72bb4195d96696a9b726264812dea9c02ffbbb04
DayDream-21 <vova13158@gmail.com>
2021-08-15 18:30:13 +0300

    Example of Facade pattern

---
+++ src/main/java/Facade/HomeTheaterTestDrive.java
@@ -0,0 +1,12 @@
+package Facade;
+
+public class HomeTheaterTestDrive {
+    public static void main(String[] args) {
+        // Создание экземпляров компонентов
+        /*HomeTheaterFacade homeTheater =
+                new HomeTheaterFacade(amp, tuner, dvd, cd,
+                        projector, screen, lights, popper);
+        homeTheater.watchMovie("Raiders of the Lost Ark");
+        homeTheater.endMovie();*/
+    }
+}
