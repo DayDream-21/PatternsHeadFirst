e1300cd14376dcaaf9528a746adefab9fb4c1f2e
DayDream-21 <vova13158@gmail.com>
2021-08-15 15:10:58 +0300

    Tested pattern Adapter for Duck->Turkey and for Iterator->Enumeration

---
+++ src/main/java/Adapter/Turkey.java
@@ -0,0 +1,18 @@
+package Adapter;
+
+public interface Turkey {
+    void gobble();
+    void fly();
+}
+
+class WildTurkey implements Turkey {
+    @Override
+    public void gobble() {
+        System.out.println("Gobble");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("I'm flying a short distance");
+    }
+}
