07d0cba1238a5d5df3d47c53564b81c9c35b2aa9
DayDream-21 <vova13158@gmail.com>
2021-08-25 14:24:52 +0300

    Added State pattern

---
+++ src/main/java/State/GumballMachineTest.java
@@ -0,0 +1,22 @@
+package State;
+
+public class GumballMachineTest {
+    public static void main(String[] args) {
+        GumballMachine gumballMachine = new GumballMachine(5);
+        System.out.println(gumballMachine);
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.refill(5);
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
+    }
+}
